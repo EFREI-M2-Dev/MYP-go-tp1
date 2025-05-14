7a55d2bc66db58d2ea2ef494c461705f34529dc3
pviprey <viprey.pierre@gmail.com>
2025-05-14 15:10:31 +0200

    initial push

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import "flag"
+
+type contact struct {
+	firstname string
+	lastname  string
+	number    string
+}
+
+func main() {
+	
+}
+
+func printContacts(contacts []contact) {}
+
+func addContact() bool {
+
+}
+
+func removeContact() contact {
+
+}
+
+func updateContact() contact {
+
+}
+
+func isNameAvailble(name string) bool {}
